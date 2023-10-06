cc40ef249b59d594eea547112b7eb2251a2a04f4
Ese Curtis <curtisese52@gmail.com>
2023-10-06 20:21:26 +0100

    feat: added models

---
+++ src/models/transactionModel.js
@@ -0,0 +1,15 @@
+// Implement transaction-related MongoDB operations here (e.g., create)
+// Example using MongoDB driver:
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectId = mongodb.ObjectId;
+
+// Replace this with actual MongoDB code
+// async function create(transaction) {
+//   // Implement transaction creation logic
+// }
+
+module.exports = {
+    // create,
+  };
+  
---
+++ src/models/userModel.js
@@ -0,0 +1,20 @@
+// Implement user-related MongoDB operations here (e.g., create, getById)
+// Example using MongoDB driver:
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectId = mongodb.ObjectId;
+
+// Replace this with actual MongoDB code
+// async function create(user) {
+//   // Implement user creation logic
+// }
+
+// async function getById(id) {
+//   // Implement get user by ID logic
+// }
+
+module.exports = {
+    // create,
+    // getById,
+  };
+  
